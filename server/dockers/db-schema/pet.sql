1a461a9c861754e181d7d0419e892316d206e5ac
Gildojp <70309279+Gildojp@users.noreply.github.com>
2022-05-20 00:30:42 +0800

    Integrated JWT

--- server/dockers/db-schema/pet.sql
+++ server/dockers/db-schema/pet.sql
@@ -16,9 +16,10 @@ CREATE TABLE cats(
 );
 CREATE TABLE users(
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(200) NOT NULL,
+    username VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL
     PRIMARY KEY (id),
-    INDEX (name)
+    INDEX (username)
 );
 
 INSERT INTO users (username, password) VALUES ('admin', 'admin');
